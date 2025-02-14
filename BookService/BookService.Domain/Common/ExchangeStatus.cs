82ff31b6f3638b0fa24554bb07cf2ca7a739e3e8
Jan Świder <jasiek.swider@outlook.com>
2025-02-14 19:36:43 +0100

    [Task-00] mobile adjust

--- BookService/BookService.Domain/Common/ExchangeStatus.cs
+++ BookService/BookService.Domain/Common/ExchangeStatus.cs
@@ -1,7 +1,4 @@
-﻿using System.Text.Json.Serialization;
-
-namespace BookService.Domain.Common;
-[JsonConverter(typeof(JsonStringEnumConverter))]
+﻿namespace BookService.Domain.Common;
 public enum ExchangeStatus
 {
     Pending = 0,
